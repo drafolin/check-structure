f39fd959a56cc2795e8e0004e88c92e6760f7085
dindin-nibnib <mail@dindin.ch>
2023-04-30 12:08:40 +0200

    added array structure check

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const checkStructureOrThrow = (a, struct, path) => {
-	for (const key in struct) {
-		if (a[key] === undefined) {
-			throw Error(`Key ${path ? `${path}.${key}` : key} is not in the source object`);
-		}
-		if (typeof a[key] === 'object') {
-			return checkStructuresOrThrow(a[key], struct[key], path ? `${path}.${key}` : key);
-		}
-		const nativeTypes = ['string', 'number', 'boolean', 'undefined', 'function'];
-		if (nativeTypes.includes(struct[key].name.toLowerCase())) {
-			if (typeof a[key] !== struct[key].name.toLowerCase()) {
-				throw Error(`Key ${path ? `${path}.${key}` : key} is not of type ${struct[key].name.toLowerCase()}`);
+	if (Array.isArray(struct)) {
+		if (!Array.isArray(a))
+			throw Error(`${path ? path : "input"} is not of type array`);
+		if (struct.length === 1) {
+			for (i in a) {
+				checkStructureOrThrow(a[i], struct[0], path ? `${path}.${key}[${i}]` : `key[${i}]`);
 			}
+		} else {
+			if (a.length !== struct.length)
+				throw Error(`Key ${path ? `${path}.${key}` : key} is not of length ${struct.length}`);
+			for (i in a)
+				checkStructureOrThrow(a[i], struct[i], path ? `${path}.${key}[${i}]` : `key[${i}]`);
 		}
+		return;
 	}
-	return;
+	if (typeof struct === "object" && struct !== null) {
+		if (typeof a !== "object" || a === null)
+			throw Error(`${path ? path : "input"} is not of type object`);
+		for (const key in struct) {
+			if (a[key] === undefined) {
+				throw Error(`Key ${path ? `${path}.${key}` : key} is not in the source object`);
+			}
+			checkStructureOrThrow(a[key], struct[key], path ? `${path}.${key}` : key);
+		}
+		return;
+	};
+
+	const nativeTypes = ['string', 'number', 'boolean', 'undefined', 'function'];
+	if (nativeTypes.includes(struct.name.toLowerCase())) {
+		if (typeof a !== struct.name.toLowerCase()) {
+			throw Error(`${path ? `${path}` : "source"} is not of type ${struct.name.toLowerCase()}`);
+		}
+		return;
+	}
 };
 
 const checkStructure = (a, struct, path) => {
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -15,6 +15,44 @@ describe("checkStructuresOrThrow", () => {
 		const b = { a: Number };
 		expect(() => checkStructureOrThrow(a, b)).to.not.throw();
 	});
+
+	it("should compare elements in an array to check if all of them follow the schema," +
+		" and throw if it doesn't fit", async () => {
+			const a = [{ a: 1 }, { a: "string" }];
+			const b = [{ a: Number }];
+			expect(() => checkStructureOrThrow(a, b)).to.throw();
+		});
+
+	it("should compare elements in an array to check if all of them follow the schema," +
+		" and not throw if it fits", async () => {
+			const a = [{ a: 1 }, { a: 2 }];
+			const b = [{ a: Number }];
+			checkStructureOrThrow(a, b);
+		});
+
+	it("should analyse deeply an object and not throw if it fits the schema", async () => {
+		const a = { a: { b: { c: 1 } } };
+		const b = { a: { b: { c: Number } } };
+		checkStructureOrThrow(a, b);
+	});
+
+	it("should analyse deeply an object and throw if it doesn't fit the schema", async () => {
+		const a = { a: { b: { c: 1 } } };
+		const b = { a: { b: { c: String } } };
+		expect(() => checkStructureOrThrow(a, b)).to.throw();
+	});
+
+	it("should check each element of an array if the schema contains more than one element", async () => {
+		const a = [{ a: 1 }, { a: "string" }];
+		const b = [{ a: Number }, { a: String }];
+		checkStructureOrThrow(a, b);
+	});
+
+	it("should check each element of an array if the schema contains more than one element and throw if it doesn't fit", async () => {
+		const a = [{ a: 1 }, { a: "string" }];
+		const b = [{ a: Number }, { a: Number }];
+		expect(() => checkStructureOrThrow(a, b)).to.throw();
+	});
 });
 
 describe("checkStructures", () => {
@@ -29,4 +67,48 @@ describe("checkStructures", () => {
 		const b = { a: Number };
 		expect(checkStructure(a, b)).to.be.true;
 	});
+
+	it("should compare elements in an array to check if all of them follow the schema," +
+		" and return false if it doesn't fit", async () => {
+			const a = [{ a: 1 }, { a: "string" }];
+			const b = [{ a: Number }];
+			expect(checkStructure(a, b)).to.be.false;
+		}
+	);
+
+	it("should compare elements in an array to check if all of them follow the schema," +
+		" and return true if it fits", async () => {
+			const a = [{ a: 1 }, { a: 2 }];
+			const b = [{ a: Number }];
+			expect(checkStructure(a, b)).to.be.true;
+		}
+	);
+
+	it("should analyse deeply an object and return true if it fits the schema", async () => {
+		const a = { a: { b: { c: 1 } } };
+		const b = { a: { b: { c: Number } } };
+		expect(checkStructure(a, b)).to.be.true;
+	}
+	);
+
+	it("should analyse deeply an object and return false if it doesn't fit the schema", async () => {
+		const a = { a: { b: { c: 1 } } };
+		const b = { a: { b: { c: String } } };
+		expect(checkStructure(a, b)).to.be.false;
+	}
+	);
+
+	it("should check each element of an array if the schema contains more than one element", async () => {
+		const a = [{ a: 1 }, { a: "string" }];
+		const b = [{ a: Number }, { a: String }];
+		expect(checkStructure(a, b)).to.be.true;
+	}
+	);
+
+	it("should check each element of an array if the schema contains more than one element and return false if it doesn't fit", async () => {
+		const a = [{ a: 1 }, { a: "string" }];
+		const b = [{ a: Number }, { a: Number }];
+		expect(checkStructure(a, b)).to.be.false;
+	}
+	);
 });
